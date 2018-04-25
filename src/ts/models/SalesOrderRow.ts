24b9f328dd0c26fe5de63dbdad03c05dccaead41
Dmitry Fokin <fokind1@yandex.ru>
2018-04-25 10:01:10 +0300

    + salesorderrows

---
+++ src/ts/models/SalesOrderRow.ts
@@ -0,0 +1,64 @@
+import { ObjectID } from "mongodb";
+import { Edm, odata } from "odata-v4-server";
+
+@Edm.Annotate({
+  term: "UI.DisplayName",
+  string: "SalesOrderRows"
+})
+export class SalesOrderRow{
+  constructor (jsonData: any) {
+    Object.assign(this, jsonData);
+  }
+
+  @Edm.Key
+  @Edm.Computed
+  @Edm.String
+  @Edm.Annotate({
+    term: "UI.DisplayName",
+    string: "ID"
+  }, {
+    term: "UI.ControlHint",
+    string: "ReadOnly"
+  })
+  _id:ObjectID
+
+  @Edm.String
+  @Edm.Annotate({
+    term: "UI.DisplayName",
+    string: "Description"
+  }, {
+    term: "UI.ControlHint",
+    string: "ShortText"
+  })
+  name:string
+
+  @Edm.Boolean
+  @Edm.Annotate({
+    term: "UI.DisplayName",
+    string: "Editing"
+}, {
+    term: "UI.ControlHint",
+    string: "Boolean"
+  })
+  edit:boolean
+
+  @Edm.Boolean
+  @Edm.Annotate({
+    term: "UI.DisplayName",
+    string: "Deleted"
+  }, {
+    term: "UI.ControlHint",
+    string: "Boolean"
+  })
+  deleted:boolean
+
+  @Edm.Double
+  @Edm.Annotate({
+    term: "UI.DisplayName",
+    string: "Total"
+  }, {
+    term: "UI.ControlHint",
+    string: "Number"
+  })
+  total:number
+}
--- src/ts/server.ts
+++ src/ts/server.ts
@@ -1,9 +1,11 @@
 import { ODataServer, ODataController, Edm, odata, ODataQuery } from "odata-v4-server";
 import { EmployeeController } from "./controllers/Employee";
 import { SalesOrderController } from "./controllers/SalesOrder";
+import { SalesOrderRowController } from "./controllers/SalesOrderRow";
 
 @odata.cors
 @odata.namespace("Erp.OData")
 @odata.controller(EmployeeController, true)
 @odata.controller(SalesOrderController, true)
+@odata.controller(SalesOrderRowController, true)
 export class ErpServer extends ODataServer{}
